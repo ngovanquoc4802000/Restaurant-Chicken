3ed6e353b065bb10194d3a776a22bd1727206fb8
Vuongnq <vuong11a3@gmail.com>
2024-12-03 23:16:36 +0700

    feat: configure environment variables for database connection and add new serve script

--- backend/database/connexion.js
+++ backend/database/connexion.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "123456",
-    database: "chicken_db"
+    password: process.env.PASSWORD ||  "",
+    database: process.env.DATABASE ||  "chicken_db",
 }) 
 
 export default pool;
