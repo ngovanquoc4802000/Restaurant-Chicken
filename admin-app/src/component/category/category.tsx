ff44a83cf66010efe4d29225a962e169cf1c8baa
vuongnq <vuongnq@fireapps.vn>
2024-12-04 10:11:17 +0700

    testing

--- admin-app/src/component/category/category.tsx
+++ admin-app/src/component/category/category.tsx
@@ -1,9 +1,9 @@
+import { faPenToSquare, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../../App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
 type CategoryType = {
   id: number;
