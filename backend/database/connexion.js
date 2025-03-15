ab78d4fbe9de9b13abd62b8eca82e14050435a73
Vuongnq <vuong11a3@gmail.com>
2025-03-15 10:31:55 +0700

    refactor database configuration to use environment variables

--- backend/database/connexion.js
+++ backend/database/connexion.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: process.env.PASSWORD ||  "123456",
-    database: process.env.DATABASE ||  "chicken_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
 }) 
 
 export default pool;
