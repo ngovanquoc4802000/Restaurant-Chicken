95fcb8f49d7007669f802fb8dfd62b906e81440f
Vuongnq <vuong11a3@gmail.com>
2025-03-15 10:41:44 +0700

    refactor database connection to use configuration object

--- backend/database/connexion.js
+++ backend/database/connexion.js
@@ -1,11 +1,15 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
+const configConnect = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
-}) 
+};
+
+console.log("configConnect", configConnect);
+
+const pool = mysql.createPool(configConnect) 
 
 export default pool;
