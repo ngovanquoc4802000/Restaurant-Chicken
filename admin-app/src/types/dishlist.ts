c0826daff2ddfc98b4f5af6cd644833fd7be4d17
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2025-04-18 11:46:23 +0700

    push dishlist vs category

---
+++ admin-app/src/types/dishlist.ts
@@ -0,0 +1,10 @@
+export interface DishTs {
+  id: number;
+  category_id: string;
+  name: string;
+  title: string;
+  currency: string;
+  price: number;
+  description: string;
+  images: string[];
+}
