64eb3e745b1ffe121bf164fac4096e2b3de6ba74
ngovanquoc <ngovanquoc480@gmail.com>
2025-06-30 15:48:48 +0700

    .

--- backend/router/category.js
+++ backend/router/category.js
@@ -3,7 +3,7 @@ import categoryControllers from '../controllers/categoryControllers.js';
 import verify from "../middleware/verifyToken.js";
 import { checkRole } from "../middleware/checkRole.js";
 const router = express.Router();
-alo
+alo 111
 
 router.get('/',categoryControllers.getCategoryAll)
 router.post('/create', verify, checkRole(["admin"]),categoryControllers.createCategory)
