f00a2910a361f597c24fde3c828b6c595400094a
VuongNQ <vuongnq@firegroup.io>
2025-06-30 13:27:00 +0700

    Remove DishList components and related services from the admin app, including create, update, and showAll functionalities, as well as associated types and services.

--- admin-app/src/services/order.ts
+++
@@ -1,7 +0,0 @@
-import { PayloadOrder, ResponseApiCreateOrder } from "../types/dishlist";
-import { RequestAxios } from "../utils/http";
-
-export const orderApi = async (payload: PayloadOrder) => {
-  const { data } = await RequestAxios.post<ResponseApiCreateOrder>(`/order/create`, payload);
-  return data;
-};
